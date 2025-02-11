0812c2463d8a874e474a9c5afaa2d699e528cf8e
Shiva Sharma <ss12@umbc.edu>
2025-02-11 00:09:03 +0000

    Added file

--- src/CheckCode.java
+++ src/CheckCode.java
@@ -1,5 +1,6 @@
 
-public class Test {
+
+public class CheckCode {
     public static void main(String[] args) {
         String str = "Sample String";
         System.out.println(str.getClass().getSimpleName());
